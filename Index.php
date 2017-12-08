12a13ad137e536ecf059d891cabf6c81ec4394e0
Ali <aaali@outlook.com>
2017-12-08 13:56:27 +0000

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php header( 'Location: /index.html' ) ;  ?>
+<html>
+  <head>
+    <title>Mystery Gift bot</title>
+    <style>
+      html,body,div,iframe {height:100%;}
+      p {position:relative;overflow:hidden;}
+      iframe {border:none;width:100%;}
+      body {margin:0;padding:0;}
+    </style>
+  </head>
+
+  <body>
+    <iframe width="100%" height="100%" frameborder="0" src="https://landbot.io/u/H-16135-KI7M8HQ1F156IBAF/index.html"></iframe>
+  </body>
+</html>
